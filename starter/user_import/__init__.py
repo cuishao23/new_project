85c9308a6b92cfb41506408af813621afe5659d0
james <1130402601@qq.com>
2021-04-25 15:45:21 +0800

    使用pandas导出excel数据

--- starter/user_import/__init__.py
+++ starter/user_import/__init__.py
@@ -7,7 +7,8 @@ import time
 from starter.config.common import LOGGING
 from starter.dao.mysql import user_session_scope, new_session_scope
 from starter.dao.user_mysql import *
-from starter.dao.new_mysql import add_g100_user_infos, get_new_user_infos, get_dec_user_infos, update_user_infos, get_user_info, get_idcard_info, update_move_member
+from starter.dao.new_mysql import add_g100_user_infos, get_new_user_infos, get_dec_user_infos, update_user_infos, \
+    get_user_info, get_idcard_info, update_move_member
 from starter.utils.des_ecb import des_encrypt
 from starter.utils.thread import mythread
 from starter.utils.idcard import get_card_info
@@ -30,29 +31,30 @@ def init_importer():
     print('user importer ok!')
 
 
-# def encrypt_des():
-#     import threading
-#     '''
-#     :手机号des-ecb加解密
-#     '''
-#     import time
-#     t1 = time.time()
-#
-#     with user_session_scope() as session:
-#         user_list = get_dec_user_infos(session)
-#
-#         pool = multiprocessing.Pool(4)    # 创建一个线程池
-#         user_data = pool.map(des_encrypt, user_list)  # 往线程池中填线程
-#         pool.close()  # 关闭线程池，不再接受线程
-#         pool.join()  # 等待线程池中线程全部执行完
-#
-#         for data in user_data:
-#             if data:
-#                 update_user_infos(session, data)
-#
-#     t2 = time.time()
-#     print('耗时:%4f' % (t2 - t1))
-#     print('encrypt_des ok!')
+def encrypt_des():
+    import threading
+    '''
+    :手机号des-ecb加解密
+    '''
+    import time
+    t1 = time.time()
+
+    with user_session_scope() as session:
+        user_list = get_dec_user_infos(session)
+
+        pool = multiprocessing.Pool(4)  # 创建一个线程池
+        user_data = pool.map(des_encrypt, user_list)  # 往线程池中填线程
+        pool.close()  # 关闭线程池，不再接受线程
+        pool.join()  # 等待线程池中线程全部执行完
+
+        for data in user_data:
+            if data:
+                update_user_infos(session, data)
+
+    t2 = time.time()
+    print('耗时:%4f' % (t2 - t1))
+    print('encrypt_des ok!')
+
 
 # def encrypt_des():
 #     '''
@@ -79,28 +81,28 @@ def init_importer():
 #
 #     print('encrypt_des ok!')
 
-def encrypt_des():
-    '''
-    :手机号des-ecb加解密
-    '''
-    t1 = time.time()
-
-    with user_session_scope() as session:
-        user_list = get_dec_t_user(session)
-
-        pool = multiprocessing.Pool(4)  # 创建一个线程池
-        user_data = pool.map(des_encrypt, user_list)  # 往线程池中填线程
-        pool.close()  # 关闭线程池，不再接受线程
-        pool.join()  # 等待线程池中线程全部执行完
-
-        for data in user_data:
-            if data:
-                update_t_user(session, data)
-
-    t2 = time.time()
-    print('耗时:%4f' % (t2 - t1))
-
-    print('encrypt_des ok!')
+# def encrypt_des():
+#     '''
+#     :手机号des-ecb加解密
+#     '''
+#     t1 = time.time()
+#
+#     with user_session_scope() as session:
+#         user_list = get_dec_t_user(session)
+#
+#         pool = multiprocessing.Pool(4)  # 创建一个线程池
+#         user_data = pool.map(des_encrypt, user_list)  # 往线程池中填线程
+#         pool.close()  # 关闭线程池，不再接受线程
+#         pool.join()  # 等待线程池中线程全部执行完
+#
+#         for data in user_data:
+#             if data:
+#                 update_t_user(session, data)
+#
+#     t2 = time.time()
+#     print('耗时:%4f' % (t2 - t1))
+#
+#     print('encrypt_des ok!')
 
 
 # def encrypt_des():
@@ -156,67 +158,37 @@ def export_data():
     :数据导出
     :return: excel
     '''
-    '''
-            move_user.id,
-            move_user.name,
-            move_user.mobile,
-            FROM_UNIXTIME( move_user.create_time ),
-            move_user_channel.openid,
-            move_user_channel.app,
-            move_user_channel.session_key,
-            move_user_channel.gid,
-            move_user_channel.extappid,
-            move_user_channel.dist,
-            move_user_channel.point,
-            move_user_channel.total_point,
-            move_user_info.uid,
-            move_user_info.role,
-            move_user_info.status,
-            move_user_info.nickname,
-            move_user_info.avatarurl,
-            move_user_info.gender,
-            move_user_info.country,
-            move_user_info.province,
-            move_user_info.city,
-            move_user_info.id_type,
-            move_user_info.id_number,
-            move_user_info.address,
-            move_user_info.reg_user_id,
-            move_user_info.jdid,
-            move_user_info.verifytime,
-            move_user_info.real_name,
-            move_user_info.real_idcard,
-            move_user_info.conflict_idcard,
-            move_user_info.phone_token,
-            move_user_info.token_time,
-            move_user_info.validity,
-            move_user_info.logindays,
-            move_user_info.email,
-            move_user_info.birth,
-            move_user_info.appid,
-            move_user_info.login_type,
-            move_user_info.login_id
-    '''
     try:
+        with user_session_scope() as session:
+            user_list = get_user_info(session)
+            print(user_list)
+
+        # pandas to_excel 100万（方法1）
+        import pandas as pd
+        df = pd.DataFrame(user_list)
+        df = df.set_index('id')
+        writer = pd.ExcelWriter(r'/Users/cuixin/Desktop/data.xlsx', engine='xlsxwriter', options={'strings_to_urls': False})  # 不将字符串转换为URL的选项创建ExcelWriter对象
+        df.to_excel(writer)
+        writer.close()
+        print('ok！')
+
         template = {
             'userInfo': {
                 'title': (
                 'id', '姓名', '手机', '创建时间', '微信登陆id', '活动代号', '登陆session', '企业id', '第三方推广应用appid', '渠道号', '当前剩余积分',
                 '用户积分', '用户uid', '角色', '状态', '昵称', '照片', '性别', '国家', '省份', '城市', '证件类型', '证件号码', '详细地址', '关联用户id',
                 'jd号', 'verifytime', 'real_name', 'real_idcard', 'conflict_idcard', 'phone_token', '在线时间', 'validity',
-                'logindays', '邮箱', '生日', 'app对应id', '登陆方式', '登陆id'),
+                'logindays', '邮箱', '生日', 'app对应id', '登陆方式', '登陆id', '区/县', '街道'),
                 'field': (
                 'id', 'name', 'mobile', 'create_time', 'openid', 'app', 'session_key', 'gid', 'extappid', 'dist',
                 'point', 'total_point', 'uid', 'role', 'status', 'nickname', 'avatarurl', 'gender', 'country',
                 'province', 'city', 'id_type', 'id_number', 'address', 'reg_user_id', 'jdid', 'verifytime', 'real_name',
                 'real_idcard', 'conflict_idcard', 'phone_token', 'token_time', 'validity', 'logindays', 'email',
-                'birth', 'appid', 'login_type', 'login_id')
+                'birth', 'appid', 'login_type', 'login_id', 'district', 'street')
             }
         }
-        with user_session_scope() as session:
-            user_list = get_user_info(session)
 
-        # 65536
+        # xlwt_to_excel 65536 （方法2）
         # inspect_result = xlwt.Workbook()
         # sheet = inspect_result.add_sheet('userInfo')
         #
@@ -229,23 +201,23 @@ def export_data():
         # os.makedirs('/Users/cuixin/Desktop/data', exist_ok=True)
         # inspect_result.save('/Users/cuixin/Desktop/data/today.xls')
 
-        # 100万
-        inspect_result = openpyxl.Workbook()
-        sheet = inspect_result.create_sheet('userInfo')
-        # sheet = inspect_result.active
-        for c, name in enumerate(template['userInfo']['title']):
-            print(c)
-            print(name)
-            sheet.cell(1, c + 1, name)
-        for r, info in enumerate(user_list):
-            for c, name in enumerate(template['userInfo']['field']):
-                value = info.get(name, '')
-                sheet.cell(r + 1, c + 1, str(value))
-        os.makedirs('/Users/cuixin/Desktop/data', exist_ok=True)
-        inspect_result.save('/Users/cuixin/Desktop/data/样例数据.xls')
-
-        logger.info('export_data ok!')
-        print('export_data ok!')
+        # openpyxl_to_excel 100万 （方法3）
+        # inspect_result = openpyxl.Workbook()
+        # sheet = inspect_result.create_sheet('userInfo')
+        # # sheet = inspect_result.active
+        # for c, name in enumerate(template['userInfo']['title']):
+        #     print(c)
+        #     print(name)
+        #     sheet.cell(1, c + 1, name)
+        # for r, info in enumerate(user_list):
+        #     for c, name in enumerate(template['userInfo']['field']):
+        #         value = info.get(name, '')
+        #         sheet.cell(r + 1, c + 1, str(value))
+        # os.makedirs('/Users/cuixin/Desktop/data', exist_ok=True)
+        # inspect_result.save('/Users/cuixin/Desktop/data/样例数据.xls')
+        #
+        # logger.info('export_data ok!')
+        # print('export_data ok!')
     except Exception as e:
         logger.error(e)
         print('export_data error=%s' % e)
@@ -305,14 +277,13 @@ def analyse_idcard():
 
     with user_session_scope() as session:
         user_list = get_idcard_info(session)
-        print(user_list)
 
-        pool = multiprocessing.Pool(multiprocessing.cpu_count())  # 创建一个线程池
-        data = pool.map(get_card_info, user_list)  # 往线程池中填线程
-        pool.close()  # 关闭线程池，不再接受线程
-        pool.join()  # 等待线程池中线程全部执行完
+        pool = multiprocessing.Pool(multiprocessing.cpu_count())
+        data = pool.map(get_card_info, user_list)
+        pool.close()
+        pool.join()
 
-        print("data==%s"%data)
+        print("data==%s" % data)
         for info in data:
             if info:
                 update_move_member(session, info)
@@ -321,6 +292,7 @@ def analyse_idcard():
     print('耗时:%4f' % (t2 - t1))
 
     print('encrypt_des ok!')
+
 
 def text():
     import openpyxl
